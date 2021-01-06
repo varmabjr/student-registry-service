a85e8456a8f58421e17a61661001315b0369b680
varmabjr@gmail.com <varmabjr@gmail.com>
2021-01-06 10:49:48 +0530

    initial commit

---
+++ src/test/java/com/student/app/servicediscovery/StudentRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.app.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
