1eb1cbdefce27eb94b362e0f92d710f70fa28306
varmabjr@gmail.com <varmabjr@gmail.com>
2021-01-06 17:38:20 +0530

    second commit

--- src/main/java/com/student/app/servicediscovery/StudentRegistryServiceApplication.java
+++ src/main/java/com/student/app/servicediscovery/StudentRegistryServiceApplication.java
@@ -1,14 +1,18 @@
 package com.student.app.servicediscovery;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@Slf4j
 public class StudentRegistryServiceApplication {
 
+
 	public static void main(String[] args) {
+		log.info("Inside the main method");
 		SpringApplication.run(StudentRegistryServiceApplication.class, args);
 	}
 
